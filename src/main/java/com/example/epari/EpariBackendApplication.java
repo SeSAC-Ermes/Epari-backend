65822f94d2c51bda136cf024231dd2682794d725
liimjiin <limiary27@gmail.com>
2024-10-30 16:06:19 +0900

    INIT: 프로젝트 backend 초기 설정 - Gradle-Groovy 프로젝트 생성 - 의존성 추가   - Lombok   - Spring Web   - Spring Data JPA   - MySQL Driver - application.properties 파일 설정

---
+++ src/main/java/com/example/epari/EpariBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.epari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpariBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpariBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/epari/EpariBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.epari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpariBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
