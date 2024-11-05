760c2c3cd02a95a3c26c39ab3b2d952dfaa948fc
jjong <whdgh9588@gmail.com>
2024-11-05 14:35:34 +0900

    FEAT: LectureRepository에 강사 정보 포함 조회 메서드 추가

--- src/main/java/com/example/epari/lecture/repository/LectureRepository.java
+++ src/main/java/com/example/epari/lecture/repository/LectureRepository.java
@@ -1,9 +1,17 @@
 package com.example.epari.lecture.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.epari.lecture.domain.Lecture;
 
+//
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
-
+	@Query("SELECT DISTINCT l FROM Lecture l " +
+		   "LEFT JOIN FETCH l.instructor " +
+		   "WHERE l.id = :lectureId")
+	Optional<Lecture> findByIdWithIdInstructor(@Param("lectureId") Long lectureId);
 }
