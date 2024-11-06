b067da8a99c83fe32ea961ccf587883581922f0b
jjong <whdgh9588@gmail.com>
2024-11-06 10:07:08 +0900

    REFACTOR: 불필요한 쿼리 삭제

--- src/main/java/com/example/epari/lecture/repository/LectureRepository.java
+++ src/main/java/com/example/epari/lecture/repository/LectureRepository.java
@@ -1,21 +1,9 @@
 package com.example.epari.lecture.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.example.epari.lecture.domain.Lecture;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
-
-	/**
-	 * LEFT JOIN FETCH를 사용하여 N+1 문제를 방지
-	 */
-	@Query("SELECT DISTINCT l FROM Lecture l " +
-		   "LEFT JOIN FETCH l.instructor " +
-		   "WHERE l.id = :lectureId")
-	Optional<Lecture> findByIdWithIdInstructor(@Param("lectureId") Long lectureId);
 
 }
