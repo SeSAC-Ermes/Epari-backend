eba948c37f4deddfe3dcf6ef55c7f3ad36d6d7ab
jjong <whdgh9588@gmail.com>
2024-11-06 12:31:38 +0900

    FEAT: LectureRepository에 강의 조회 기능 추가 - 학생 ID로 강의 조회 및 강사 ID로 강의 조회 기능 추가

--- src/main/java/com/example/epari/lecture/repository/LectureRepository.java
+++ src/main/java/com/example/epari/lecture/repository/LectureRepository.java
@@ -1,6 +1,10 @@
 package com.example.epari.lecture.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.epari.lecture.domain.Lecture;
 
@@ -9,5 +13,20 @@ import com.example.epari.lecture.domain.Lecture;
  * JPA를 사용해 강의 Entity의 데이터베이스 연산을 담당합니다.
  */
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
+
+	/**
+	 * 학생 id를 받아 강의 id 와 학생 id 일치로 조회
+	 */
+	@Query("SELECT l FROM Lecture l "
+		   + "INNER JOIN LectureStudent ls ON l.id = ls.lecture.id "
+		   + "WHERE ls.student.id = :studentId")
+	List<Lecture> findAllByStudentId(@Param("studentId") Long studentId);
+
+	/**
+	 * 강사 id를 받아 강의 테이블에서 일치하는지 확인
+	 */
+	@Query("SELECT l FROM Lecture l "
+		   + "WHERE l.instructor.id = :instructorId")
+	List<Lecture> findAllByInstructorId(@Param("instructorId") Long instructorId);
 
 }
