ae1b8ac7b39b10b61caa4640d082dcd15d2903af
jjong <whdgh9588@gmail.com>
2024-11-07 16:28:07 +0900

    REFACTOR: LectureController에 권한 설정 추가

--- src/main/java/com/example/epari/lecture/controller/LectureController.java
+++ src/main/java/com/example/epari/lecture/controller/LectureController.java
@@ -3,6 +3,7 @@ package com.example.epari.lecture.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.epari.global.common.enums.UserRole;
+import com.example.epari.global.annotation.CurrentUserEmail;
 import com.example.epari.lecture.dto.lecture.LectureRequestDto;
 import com.example.epari.lecture.dto.lecture.LectureResponseDto;
 import com.example.epari.lecture.service.LectureService;
@@ -28,9 +29,11 @@ public class LectureController {
 	private final LectureService lectureService;
 
 	/**
+	 * 강의 생성
 	 * 강의 id로 조회
 	 */
 	@PostMapping
+	@PreAuthorize("hasRole('INSTRUCTOR')")
 	public ResponseEntity<LectureResponseDto> createLecture(
 			@RequestParam Long instructorId,
 			@RequestBody LectureRequestDto request) {
@@ -38,9 +41,10 @@ public class LectureController {
 	}
 
 	/**
-	 * 강의 id로 삭제
+	 * 강의 id로 조회 상세 페이지 이동 시
 	 */
 	@GetMapping("/{id}")
+	@PreAuthorize("hasAnyRole('INSTRUCTOR', 'STUDENT')")
 	public ResponseEntity<LectureResponseDto> getLecture(@PathVariable Long id) {
 		return ResponseEntity.ok(lectureService.getLecture(id));
 	}
@@ -49,15 +53,11 @@ public class LectureController {
 	 * 사용자 역할에 따른 강의 목록 조회
 	 */
 	@GetMapping("/userlectures")
+	@PreAuthorize("hasAnyRole('INSTRUCTOR', 'STUDENT')")
 	public ResponseEntity<List<LectureResponseDto>> getMyLectures(
-			@RequestParam UserRole role,
-			@RequestParam Long userId) {
+			@CurrentUserEmail String email) {
 
-		List<LectureResponseDto> lectures = switch (role) {
-			case INSTRUCTOR -> lectureService.getInstructorLectures(userId);
-			case STUDENT -> lectureService.getStudentLectures(userId);
-		};
-
+		List<LectureResponseDto> lectures = lectureService.getMyLectures(email);
 		return ResponseEntity.ok(lectures);
 	}
 
@@ -66,6 +66,7 @@ public class LectureController {
 	 * 강사 id로 검증
 	 */
 	@PutMapping("/{id}")
+	@PreAuthorize("hasRole('INSTRUCTOR')")
 	public ResponseEntity<LectureResponseDto> updateLecture(
 			@PathVariable Long id,
 			@RequestParam Long instructorId,
@@ -79,6 +80,7 @@ public class LectureController {
 	 * 강사 id로 검증
 	 */
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('INSTRUCTOR')")
 	public ResponseEntity<Void> deleteLecture(
 			@PathVariable Long id,
 			@RequestParam Long instructorId
