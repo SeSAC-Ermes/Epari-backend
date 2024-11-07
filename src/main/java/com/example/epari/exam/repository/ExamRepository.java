0f50f4a202515ade0204562035406a07b75c819d
hyerin315 <lhl9603@gmail.com>
2024-11-07 16:00:26 +0900

    FEAT: ExamRepository에 특정 강의에 속한 시험 조회 기능 추가
    - ExamRepository에 특정 강의에 속한 모든 시험을 조회하는 findByLectureId 메소드 추가

--- src/main/java/com/example/epari/exam/repository/ExamRepository.java
+++ src/main/java/com/example/epari/exam/repository/ExamRepository.java
@@ -1,13 +1,17 @@
 package com.example.epari.exam.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.epari.exam.domain.Exam;
 
 /**
  * 시험 정보에 대한 데이터베이스 접근을 담당하는 Repository 인터페이스
- * - Spring Data JPA를 통해 기본적인 CRUD 연산을 제공
  */
 public interface ExamRepository extends JpaRepository<Exam, Long> {
+
+	// 특정 강의에 속한 모든 시험 조회
+	List<Exam> findByLectureId(Long lectureId);
 
 }
