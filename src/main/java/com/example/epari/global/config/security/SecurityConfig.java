7f6045076e31a249424aad998738b2fbd3ef3fd2
hyerin315 <lhl9603@gmail.com>
2024-11-07 12:08:58 +0900

    MERGE: 메인 브랜치 설정값 복구
    - filterChain 설정값 복구

--- src/main/java/com/example/epari/global/config/security/SecurityConfig.java
+++ src/main/java/com/example/epari/global/config/security/SecurityConfig.java
@@ -45,8 +45,6 @@ public class SecurityConfig {
 						.requestMatchers("/api/instructor/**").hasRole("INSTRUCTOR")
 						.requestMatchers("/api/student/**").hasRole("STUDENT")
 						.anyRequest().authenticated()
-						.anyRequest().permitAll() // TODO: 추후 권한별 엔드포인트 설정하기
-
 				)
 				.oauth2ResourceServer(oauth2 -> oauth2
 						.jwt(jwt -> jwt
