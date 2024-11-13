908e9ad716cc562f8d9e6f15c143879fcb453e89
Chan-GN <ocg0229@gmail.com>
2024-11-13 18:30:13 +0900

    COMMIT: CourseAccessDeniedException 클래스 레벨 주석 추가

--- src/main/java/com/example/epari/global/exception/course/CourseAccessDeniedException.java
+++ src/main/java/com/example/epari/global/exception/course/CourseAccessDeniedException.java
@@ -3,6 +3,9 @@ package com.example.epari.global.exception.course;
 import com.example.epari.global.exception.BusinessBaseException;
 import com.example.epari.global.exception.ErrorCode;
 
+/**
+ * 강의에 대한 접근 권한이 없는 경우 발생하는 예외
+ */
 public class CourseAccessDeniedException extends BusinessBaseException {
 
 	public CourseAccessDeniedException() {
