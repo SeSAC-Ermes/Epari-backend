ef55f89c63c9aa6fd51e992ba86cfdfc934b1bda
씅찬 <151029131+sseung519@users.noreply.github.com>
2024-11-14 16:27:57 +0900

    [FEAT] 과제 생성 시 S3 파일 업로드 기능 구현 (#60)
    * FEAT: 과제 생성 시 S3 파일 업로드 기능 추가
    - @ModelAttribute를 사용해 addAssignment 메서드에서 파일 업로드 처리
    - 과제 데이터와 파일 데이터를 통합하여 일관성 있게 요청 처리
    
    * FEAT: AssignmentRequestDto에 파일 지원 추가
    - 과제 요청에 파일 데이터를 포함할 수 있도록 List<MultipartFile> files 필드 추가
    - 파일 처리를 위한 getter와 setter 추가
    
    * FEAT: 서비스 레이어에 S3 파일 업로드 및 과제 연동 기능 구현
    - addAssignment 메서드에서 S3에 파일 업로드 기능 추가
    - 업로드된 파일을 과제와 연결하고 관계를 관리하도록 수정
    - AssignmentFile 엔티티를 활용한 과제-파일 매핑 설정
    
    * FEAT: Assignment와 AssignmentFile 간의 관계 매핑 추가
    - @ManyToOne 관계를 Assignment와 설정
    - 양방향 관계를 관리하기 위해 setAssignment 메서드 수정
    
    * MERGE: .DS_Store를 .gitignore에 추가
    
    * CHORE: .gitignore .DS_Store 추가
    
    * CHORE: .DS_Store git 추적 제거
    
    * CHORE: src/.DS_Store git 추적 제거

--- src/main/java/com/example/epari/assignment/dto/assignment/AssignmentRequestDto.java
+++ src/main/java/com/example/epari/assignment/dto/assignment/AssignmentRequestDto.java
@@ -1,10 +1,12 @@
 package com.example.epari.assignment.dto.assignment;
 
-import java.time.LocalDateTime;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 과제 생성 및 수정을 위한 요청 DTO
@@ -19,5 +21,8 @@ public class AssignmentRequestDto {
 	private String description;     // 과제 설명
 
 	private LocalDateTime deadline; // 마감기한
-	
+
+	private List<MultipartFile> files;
+
+
 }
