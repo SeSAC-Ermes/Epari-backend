78f414aaed2137de1581eff03e516169454d1862
jjong <whdgh9588@gmail.com>
2024-11-18 12:32:54 +0900

    REFACTOR: 불필요한 import 문 제거

--- src/main/java/com/example/epari/course/controller/CourseContentController.java
+++ src/main/java/com/example/epari/course/controller/CourseContentController.java
@@ -3,7 +3,6 @@ package com.example.epari.course.controller;
 import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.example.epari.course.dto.content.CourseContentRequestDto;
 import com.example.epari.course.dto.content.CourseContentResponseDto;
