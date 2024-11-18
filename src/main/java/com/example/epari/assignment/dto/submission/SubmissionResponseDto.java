951d496e63085fc563977566dc83ac622e9a930d
씅찬 <htc_desirehd@naver.com>
2024-11-19 00:27:50 +0900

    FEAT: 과제 제출 관련 Dto

---
+++ src/main/java/com/example/epari/assignment/dto/submission/SubmissionResponseDto.java
@@ -0,0 +1,67 @@
+package com.example.epari.assignment.dto.submission;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.epari.assignment.domain.Submission;
+import com.example.epari.assignment.dto.file.SubmissionFileResponseDto;
+import com.example.epari.user.domain.Student;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SubmissionResponseDto {
+
+	private Long id;
+
+	private String description;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	private List<SubmissionFileResponseDto> files;
+
+	private StudentInfo student;
+
+	@Getter
+	@Builder
+	public static class StudentInfo {
+
+		private Long id;
+
+		private String name;
+
+		private String email;
+
+		private String phoneNumber;
+
+		public static StudentInfo from(Student student) {
+			return StudentInfo.builder()
+					.id(student.getId())
+					.name(student.getName())
+					.email(student.getEmail())
+					.phoneNumber(student.getPhoneNumber())
+					.build();
+		}
+
+	}
+
+	public static SubmissionResponseDto from(Submission submission) {
+		return SubmissionResponseDto.builder()
+				.id(submission.getId())
+				.description(submission.getDescription())
+				.createdAt(submission.getCreatedAt())
+				.updatedAt(submission.getUpdatedAt())
+				.files(submission.getFiles().stream()
+						.map(SubmissionFileResponseDto::new)
+						.collect(Collectors.toList()))
+				.student(submission.getStudent() != null ?
+						StudentInfo.from(submission.getStudent()) : null)
+				.build();
+	}
+
+}
