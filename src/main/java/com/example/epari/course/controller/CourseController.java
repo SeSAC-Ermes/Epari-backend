831a12a323bd84e8ab87a43f1682042474d42bcd
jjong <whdgh9588@gmail.com>
2024-11-18 12:29:39 +0900

    REFACTOR: 불필요한 @EnableMethodSecurity 어노테이션 제거
    - 클래스 레벨의 @EnableMethodSecurity 어노테이션 제거
    - SecurityConfig에서 전역으로 메서드 보안을 활성화하므로 중복 설정 제거

--- src/main/java/com/example/epari/course/controller/CourseController.java
+++ src/main/java/com/example/epari/course/controller/CourseController.java
@@ -25,7 +25,6 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/courses")
 @RequiredArgsConstructor
-@EnableMethodSecurity
 public class CourseController {
 
 	private final CourseService courseService;
