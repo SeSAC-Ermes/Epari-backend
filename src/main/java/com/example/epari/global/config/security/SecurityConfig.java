88bb530f7cae775d627c1c1b4bed031aaf9f1320
jjong <whdgh9588@gmail.com>
2024-11-18 12:30:48 +0900

    FEAT: 메서드 수준 보안 설정 추가
    - @EnableMethodSecurity 어노테이션 추가로 메서드 수준 보안 활성화
    - @PreAuthorize 어노테이션 사용을 위한 설정 추가

--- src/main/java/com/example/epari/global/config/security/SecurityConfig.java
+++ src/main/java/com/example/epari/global/config/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -31,6 +32,7 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	private final CorsProperties corsProperties;
