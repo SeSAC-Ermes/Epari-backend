b9dc4dc30c89d21c85cc3cf6c92217a67805658c
hyerin315 <lhl9603@gmail.com>
2024-11-18 19:08:32 +0900

    FEAT: 시험 목록 조회 메소드 수정
    - ExamRepository에 findByCourseIdOrderByExamDateTimeDesc 구현
    - findByInstructorEmail, findByStudentEmail에 변수를 @Param 형태로 변경

--- src/main/java/com/example/epari/exam/repository/ExamRepository.java
+++ src/main/java/com/example/epari/exam/repository/ExamRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.epari.exam.domain.Exam;
 
@@ -16,21 +17,28 @@ public interface ExamRepository extends JpaRepository<Exam, Long> {
 	// 특정 강의에 속한 모든 시험 조회
 	List<Exam> findByCourseId(Long courseId);
 
+	// 특정 강의의 시험 목록 조회
+	@Query("SELECT e FROM Exam e " +
+			"WHERE e.course.id = :courseId " +
+			"ORDER BY e.examDateTime DESC")
+	List<Exam> findByCourseIdOrderByExamDateTimeDesc(@Param("courseId") Long courseId);
+
 	// 특정 강의에 속한 시험 상세 조회
 	Optional<Exam> findByCourseIdAndId(Long courseId, Long id);
 
-	// 강사가 담당하는 강의의 모든 시험 조회
-	@Query("SELECT e FROM Exam e "
-			+ "JOIN FETCH e.course c "
-			+ "JOIN c.instructor i "
-			+ "WHERE i.email = :instructorEmail")
-	List<Exam> findByInstructorEmail(String instructorEmail);
+	// 강사가 출제한 시험 목록 조회
+	@Query("SELECT e FROM Exam e " +
+			"JOIN FETCH e.course c " +
+			"JOIN c.instructor i " +
+			"WHERE i.email = :instructorEmail")
+	List<Exam> findByInstructorEmail(@Param("instructorEmail") String instructorEmail);
 
-	// 학생이 수강중인 강의의 모든 시험 조회
-	@Query("SELECT e FROM Exam e "
-			+ "JOIN FETCH e.course c "
-			+ "JOIN CourseStudent cs ON cs.course = c "
-			+ "WHERE cs.student.email = :studentEmail")
-	List<Exam> findByStudentEmail(String studentEmail);
+	// 학생이 수강중인 강의의 시험 목록 조회
+	@Query("SELECT DISTINCT e FROM Exam e " +
+			"JOIN FETCH e.course c " +
+			"JOIN c.courseStudents cs " +
+			"JOIN cs.student s " +
+			"WHERE s.email = :studentEmail")
+	List<Exam> findByStudentEmail(@Param("studentEmail") String studentEmail);
 
 }
