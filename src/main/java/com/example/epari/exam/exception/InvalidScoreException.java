c7bc57df378debd8264727e1d5cd1fc1b0502ee2
hyerin315 <lhl9603@gmail.com>
2024-11-20 11:36:30 +0900

    FEAT: 채점 관리 ErrorCode 및 예외 클래스 생성
    - ErrorCode에 채첨 관련 에러 메세지 정의
    - 채점 예외 처리 클래스 생성
     - GradingException
     - GradingNotPossibleException
     - InvalidScoreException

---
+++ src/main/java/com/example/epari/exam/exception/InvalidScoreException.java
@@ -0,0 +1,8 @@
+package com.example.epari.exam.exception;
+
+public class InvalidScoreException extends GradingException {
+    
+    public InvalidScoreException(String message) {
+        super(message);
+    }
+}
