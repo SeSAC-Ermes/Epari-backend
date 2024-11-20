392d960742dbcb29e000867074f7cbd9b5425dfe
sseung519 <htc_desirehd@naver.com>
2024-11-20 12:34:16 +0900

    FEAT: 특정 과제의 특정 학생 제출물을 가져오는 쿼리문

--- src/main/java/com/example/epari/assignment/repository/SubmissionRepository.java
+++ src/main/java/com/example/epari/assignment/repository/SubmissionRepository.java
@@ -3,7 +3,11 @@ package com.example.epari.assignment.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.epari.assignment.domain.Submission;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-
+	@Query("SELECT s FROM Submission s WHERE s.assignment.id = :assignmentId AND s.student.id = :studentId")
+	Optional<Submission> findByAssignment_IdAndStudent_Id(Long assignmentId, Long studentId);
 }
