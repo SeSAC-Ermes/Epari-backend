bf8527edfed967c32ef2e4480d1e26f3d30d8a31
sseung519 <htc_desirehd@naver.com>
2024-11-26 14:42:44 +0900

    FEAT: 과제 채점 관련 email Enum

--- src/main/java/com/example/epari/global/event/NotificationType.java
+++ src/main/java/com/example/epari/global/event/NotificationType.java
@@ -10,7 +10,10 @@ public enum NotificationType {
 
 	STUDENT_APPROVED("수강생 승인", "student-approved.html"),
 	INSTRUCTOR_APPROVED("강사 승인", "instructor-approved.html"),
-	USER_REJECTED("사용자 반려", "user-rejected.html");
+	USER_REJECTED("사용자 반려", "user-rejected.html"),
+	// 과제 채점 알림
+	PASS("통과", "assignment-pass.html"),
+	NONE_PASS("미통과", "assignment-none-pass.html");
 
 	private final String description;
 
